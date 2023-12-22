85ef83af586ad6968dd0306a3fc5f0d15ed614ee
d.zakharchuk <d.zakharchuk@smartweb.com.ua>
2023-12-22 12:49:45 +0200

    VID-3001 simplify proto gen, update protobufs

---
+++ README.md
@@ -0,0 +1,10 @@
+FeatureFlags service protobuf files
+
+Overview
+--------
+
+To generate Python code from protobuf files run:
+- ``pdm run protoc``
+
+To install package:
+- TODO
--- scripts/protoc.sh
+++ scripts/protoc.sh
@@ -10,10 +10,10 @@ python3 -m grpc_tools.protoc \
   --python_out=. \
   featureflags_protobuf/*.proto
 
-#  --python_grpc_out=featureflags_protobuf \
 python3 -m grpc_tools.protoc \
   --proto_path=. \
   --proto_path="$HIKU_PROTO_PATH" \
+  --python_out=. \
+  --python_grpc_out=. \
   --grpc_python_out=. \
-  --grpclib_python_out=. \
   featureflags_protobuf/service.proto
